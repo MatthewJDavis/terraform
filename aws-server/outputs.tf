748c89ea9b552c04a902b864d93e9e647d8558a4
matthewjdavis <matthewdavis111@gmail.com>
2017-05-21 14:12:21 +0100

    added public dns to output

--- aws-server/outputs.tf
+++ aws-server/outputs.tf
@@ -2,6 +2,10 @@ output "ami" {
   value = "${lookup(var.amis, var.region)}"
 }
 
-output "Public IP" {
+output "public_ip" {
     value = "${aws_eip.ip.public_ip}"
+}
+
+output "public_dns" {
+    value = "${aws_instance.server.public_dns}"
 }
