b7b72a36c3e53ddafe074f55b34496e8b28db5b5
= <matthewdavis111@gmail.com>
2018-06-09 22:08:18 -0400

    added rundeck security rule

--- azure-linux-server/main.tf
+++ azure-linux-server/main.tf
@@ -42,6 +42,18 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "Rundeck"
+    priority                   = 1101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "4440"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags {
     environment = "Terraform Demo"
   }
