4c01b0c778df9279eab91c1c71ddf51045b39bdd
= <matthewdavis111@gmail.com>
2018-11-26 02:45:05 +0000

    testing with service p rg deploy

--- azure-windows-server/main.tf
+++ azure-windows-server/main.tf
@@ -1,16 +1,8 @@
- module "windowsservers" {
-    source              = "Azure/compute/azurerm"
-    location            = "West US 2"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
-    vm_os_simple        = "WindowsServer"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
-  }
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "uksouth"
 
-  module "vnet" {
-    source              = "Azure/vnet/azurerm"
-    version             = "~> 1.0.0"
-    location            = "West US 2"
-    resource_group_name = "terraform-vm"
-  }
+    tags {
+        environment = "Terraform Demo"
+    }
+}
