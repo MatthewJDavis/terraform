44ad2bf9416db31668391579528c71da8dcc5686
= <matthewdavis111@gmail.com>
2018-11-27 00:30:17 +0000

    added vars

--- azure-windows-server/main.tf
+++ azure-windows-server/main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    tenant_id = "${var.tenant_id}"
+    client_id = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+}
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "myResourceGroup"
     location = "uksouth"
