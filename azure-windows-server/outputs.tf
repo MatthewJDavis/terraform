e69594cd868d9e5e5905ac754307ca3507a075c9
= <matthewdavis111@gmail.com>
2018-11-28 02:18:03 +0000

    updated vars

--- azure-windows-server/outputs.tf
+++ azure-windows-server/outputs.tf
@@ -1,6 +1,3 @@
-output "PublicIP" {
+output "ip" {
   value = "${azurerm_public_ip.myterraformpublicip.ip_address}"
-}
-output "PublicDNS" {
-  value = "${azurerm_public_ip.myterraformpublicip.domain_name_label}"
 }
--- azure-windows-server/pester-tests/AzureWindowsServer.Tests.Tests.ps1
+++ azure-windows-server/pester-tests/AzureWindowsServer.Tests.Tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-$ResourceGroupName = 'myResourceGroup'
+$ResourceGroupName = 'poshbot'
 $Location = 'uksouth'
 $VMName = 'poshbot'
 $requiredSize = 'Standard_A1_V2'
