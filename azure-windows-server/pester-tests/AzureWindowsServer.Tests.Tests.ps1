875f19f34d33ae66e99f2b94e89ffd7d1cec976c
= <matthewdavis111@gmail.com>
2018-11-28 00:47:03 +0000

    updated tests

--- azure-windows-server/pester-tests/AzureWindowsServer.Tests.Tests.ps1
+++ azure-windows-server/pester-tests/AzureWindowsServer.Tests.Tests.ps1
@@ -4,14 +4,26 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 $ResourceGroupName = 'myResourceGroup'
 $Location = 'uksouth'
+$VMName = 'poshbot'
+$requiredSize = 'Standard_A1_V2'
 
 $rg = Get-AzResourceGroup | Where-Object -Property ResourceGroupName -EQ $ResourceGroupName
-Describe "AzureWindowsServer.Tests" {
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName
+Describe "Poshbot Server infrastructure" {
+  Context "Resource Group" {
     It "Checks that we have a created resource group" {
-        $null -eq $rg | Should -Be $false
+      $null -eq $rg | Should -Be $false
     }
-    It "Resource group is created in the location: $uksouth" {
-        $rg.location | Should -Be $Location
+    It "Resource group is created in the location: $location" {
+      $rg.location | Should -Be $Location
     }
-
+  }
+  Context "VM" {
+    It "Should be in the location: $location" {
+        $vm.location | Should -Be $location
+    }  
+    It "Should be the Size $requiredSize" {
+      $vm.HardwareProfile.VmSize | Should -Be $requiredSize
+    }
+  }
 }
