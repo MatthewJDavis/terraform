a1cc632cd82b136ec27f3f2d6bdccb5410f6c738
MatthewJDavis <matthewdavis111@gmail.com>
2019-09-04 22:18:19 -0400

    add terraform storage

---
+++ azure-setup-demo/New-TerraformStorage.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+  Short description
+.DESCRIPTION
+  Long description
+.EXAMPLE
+  PS C:\> <example usage>
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  General notes
+#>
+
+$Location = 'northeurope'
+$RGName = 'terraform-state'
+$StorageAccountName = 'mdterraform'
+$Tags = @{'Project' = 'terraform'; 'Environment' = 'Demo' }
+$ContainerName = 'demo-state'
+
+New-AzResourceGroup -Name $RGName -Location $Location -Tag $tags
+
+$storageParams = @{
+  'ResourceGroupName'      = $RGName 
+  'Name'                   = $StorageAccountName 
+  'SkuName'                = 'Standard_LRS' 
+  'EnableHttpsTrafficOnly' = $true 
+  'Location'               = $Location 
+  'Kind'                   = 'BlobStorage' 
+  'AccessTier'             = 'Hot'
+  'Tag'                    = $tags
+}
+
+New-AzStorageAccount @storageParams
+
+#region storageContext
+$key = (Get-AzStorageAccountKey -ResourceGroupName $RGName -Name $StorageAccountName)[0].value
+
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $key
+#endregion storageContext
+
+#region container
+New-AzStorageContainer -Name $ContainerName -Permission Off -Context $ctx
+#endregion container
+
+New-AzStorageAccountSASToken -Service Blob -Permission rwdl -Context $ctx -Protocol HttpsOnly -ResourceType Service, Container, Object
+
