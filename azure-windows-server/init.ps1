c09deeee738b47100eba51cc8fdf41d658031f8e
MatthewJDavis <matthewdavis111@gmail.com>
2020-01-12 16:26:05 -0500

    comments and tidy

--- azure-windows-server/init.ps1
+++ azure-windows-server/init.ps1
@@ -1,17 +1,18 @@
-$certPath = 'C:\temp\cert.pem'
+# Script that will set up a Windows server 2016 for https WinRM remoting with a user certificate.
+# Self signed cert generated on the server used for listener
 
+#region initial setup, get public cert of user
+$certPath = 'C:\temp\cert.pem'
 New-Item C:\temp -ItemType Directory
 $uri = 'https://raw.githubusercontent.com/MatthewJDavis/terraform/master/azure-windows-server/cert.pem'
 Invoke-WebRequest -Uri $uri -OutFile $certPath
+#endregion
 
 #region Set up WinRM Https listener with self signed cert
-
 $SubjectName = $env:ComputerName
-$CertValiditDays = 1095
 $CertStoreLocation = 'Cert:\LocalMachine\My'
 
 $cert = New-SelfSignedCertificate -DnsName $env:ComputerName -CertStoreLocation $CertStoreLocation 
-
 
 # Create the hashtables of settings to be used.
 $valueset = @{
@@ -27,17 +28,13 @@ $selectorset = @{
 Write-Verbose "Enabling SSL listener."
 New-WSManInstance -ResourceURI 'winrm/config/Listener' -SelectorSet $selectorset -ValueSet $valueset
 
-netsh advfirewall firewall add rule name="WinRM-HTTPS-test" dir=in localport=5986 protocol=TCP action=allow
-
-
+# set firewall
 $fwRuleName = 'winRM-HTTPS'
 New-NetFirewallRule -Name $fwRuleName -DisplayName $fwRuleName -Direction Inbound -LocalPort 5986 -Protocol TCP -Action Allow -Enabled True -Profile Any
 
 #endregion
 
-
-
-# import the user cert
+#region import user cert
 
 $cert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2
 $cert.Import("$certPath")
@@ -59,9 +56,9 @@ $store = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509
 $store.Open("MaxAllowed")
 $store.Add($cert)
 $store.Close()
+#endregion
 
-
-# create local user
+#region local user
 Add-Type -AssemblyName System.Web
 $username = "ansible"
 $generatedPassword = [System.Web.Security.Membership]::GeneratePassword(20, 0)
@@ -72,13 +69,9 @@ $credential = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 
 New-LocalUser -Name $username -AccountNeverExpires -Description 'ansible user' -PasswordNeverExpires -Password $credential.Password
 Add-LocalGroupMember -Group Administrators -Member 'ansible'
+#endregion
 
-
-# Add cert to endpoint for user
-
-# This is the issuer thumbprint which in the case of a self generated cert
-# is the public key thumbprint, additional logic may be required for other
-# scenarios
+#region certificate for WinRM endpoint
 $thumbprint = (Get-ChildItem -Path cert:\LocalMachine\root | Where-Object { $_.Subject -eq "CN=$username" }).Thumbprint
 
 New-Item -Path WSMan:\localhost\ClientCertificate `
@@ -88,4 +81,5 @@ New-Item -Path WSMan:\localhost\ClientCertificate `
   -Credential $credential `
   -Force
 
-  Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+#endregion
